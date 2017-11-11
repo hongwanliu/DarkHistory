6f3c2ba92174311af21b40d06d30f133e90af505
Hongwan Liu <hongwan@mit.edu>
2017-11-10 19:05:13 -0500

    engloss_spec_diff fixed, checked
    New ics_engloss_spectrum.py

--- darkhistory/electrons/ics/engloss_diff_terms.py
+++ darkhistory/electrons/ics/engloss_diff_terms.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy.special as sp
 
 from darkhistory.utilities import log_1_plus_x
+from darkhistory.electrons.ics import bose_einstein_integrals as BE_int
 
 def F1_up_down(beta, delta, T, as_pairs=False):
     """ Computes the F1_upscatter - F1_downscatter term.
@@ -69,21 +70,12 @@ def F1_up_down(beta, delta, T, as_pairs=False):
 
     prefac = 8*beta**3/3 + 28*beta**5/15
 
-    if as_pairs:
 
-        return prefac*(
-            term_eta*eta + term_eta_3*eta**3 + term_eta_5*eta**5
+    return np.transpose(
+        prefac*np.transpose(
+            eta*term_eta + eta**3*term_eta_3 + eta**5*term_eta_5
         )
-
-    else:
-
-        return np.transpose(
-            prefac*np.transpose(
-                np.outer(term_eta, eta) 
-                + np.outer(term_eta_3, eta**3)
-                + np.outer(term_eta_5, eta**5)
-            )
-        )
+    )
 
 def F0_up_down_diff(beta, delta, T, as_pairs=False):
     """ Computes the F0_upscatter - F0_downscatter term.
@@ -150,21 +142,11 @@ def F0_up_down_diff(beta, delta, T, as_pairs=False):
 
     prefac = -2*beta**2 - beta**4
 
-    if as_pairs:
-
-        return prefac*(
-            term_eta_2*eta**2 + term_eta_4*eta**4 + term_eta_6*eta**6
+    return np.transpose(
+        prefac*np.transpose(
+            eta**2*term_eta_2 + eta**4*term_eta_4 + eta**6*term_eta_6
         )
-
-    else:
-
-        return np.transpose(
-            prefac*np.transpose(
-                np.outer(term_eta_2, eta**2) 
-                + np.outer(term_eta_4, eta**4)
-                + np.outer(term_eta_6, eta**6)
-            )
-        )
+    )
 
 def F0_up_down_sum(beta, delta, T, as_pairs=False):
     """ Computes the F0_upscatter + F0_downscatter term.
@@ -227,21 +209,11 @@ def F0_up_down_sum(beta, delta, T, as_pairs=False):
 
     prefac = 2*beta**3/3 + 13*beta**5/15
 
-    if as_pairs:
-
-        return prefac*(
-            term_eta*eta + term_eta_3*eta**3 + term_eta_5*eta**5
+    return np.transpose(
+        prefac*np.transpose(
+            eta*term_eta + eta**3*term_eta_3 + eta**5*term_eta_5
         )
-
-    else:
-
-        return np.transpose(
-            prefac*np.transpose(
-                np.outer(term_eta, eta) 
-                + np.outer(term_eta_3, eta**3)
-                + np.outer(term_eta_5, eta**5)
-            )
-        )
+    )
 
 def F_inv_up_down(beta, delta, T, as_pairs=False):
     """ Computes the F_inv_upscatter - F_inv_downscatter term.
@@ -315,24 +287,15 @@ def F_inv_up_down(beta, delta, T, as_pairs=False):
 
     prefac = beta - beta**3/2
 
-    if as_pairs:
 
-        return prefac*(
-            term_eta_3*eta**3 + term_eta_5*eta**5 + term_eta_7*eta**7
+    return np.transpose(
+        prefac*np.transpose( 
+            eta**3*term_eta_3 + eta**5*term_eta_5 + eta**7*term_eta_7
         )
+    )
 
-    else:
-
-        return np.transpose(
-            prefac*np.transpose( 
-                + np.outer(term_eta_3, eta**3)
-                + np.outer(term_eta_5, eta**5)
-                + np.outer(term_eta_7, eta**7)
-            )
-        )
-
-def F_inv2_up_down(beta, delta, T, as_pairs=False):
-    """ Computes the F_inv2_upscatter + F_inv2_downscatter term.
+def F_rem(beta, delta, T, as_pairs=False):
+    """ Computes the F_rem term.
 
     This term is used in the small parameter expansion method for computing the ICS energy loss spectrum. 
 
@@ -363,62 +326,84 @@ def F_inv2_up_down(beta, delta, T, as_pairs=False):
     large = A_all > 0.01
     small = ~large
 
-    term_eta_4 = np.zeros_like(A_all)
-    term_eta_6 = np.zeros_like(A_all)
-    term_eta_8 = np.zeros_like(A_all)
+    F_inv_3_int = BE_int.F_inv_3(A_all, np.ones_like(A_all)*np.inf)
+
+    F_inv_5_int = BE_int.F_inv_5(A_all, np.ones_like(A_all)*np.inf)
+
+
+    term_eta_4  = np.zeros_like(A_all) + F_inv_3_int[0]/6
+    term_eta_6  = np.zeros_like(A_all) + F_inv_5_int[0]/15
+    term_eta_8  = np.zeros_like(A_all)
+    term_eta_10 = np.zeros_like(A_all)
 
     if np.any(large):
 
         A = A_all[large]
 
-        term_eta_4[large] = (np.exp(-A)/A**2)/(1 - np.exp(-A))
+        term_eta_4[large] += -(1/6)*(np.exp(-A)/A**2)/(1 - np.exp(-A))
 
-        term_eta_6[large] = (
-            (A**2 + 4*A + 6)*np.exp(-A) + (A**2 - 4*A - 12)*np.exp(-2*A)
-            + 6*np.exp(-3*A)    
-        )/(24*A**4*(1 - np.exp(-A))**3)
+        term_eta_6[large] += (
+            - (5*A**2 + 5*A + 21)*np.exp(-A)
+            + (-5*A**2 + 5*A + 42)*np.exp(-2*A)
+            - 21*np.exp(-3*A)
+        )/(720 * A**4 * (1 - np.exp(-A))**3)
 
-        term_eta_8[large] = (
-            (A**4 + 8*A**3 + 36*A**2 + 96*A + 120)*np.exp(-A)
-            + (11*A**4 + 24*A**3 - 36*A**2 - 288*A - 480)*np.exp(-2*A)
-            + (11*A**4 - 24*A**3 - 36*A**2 + 288*A + 720)*np.exp(-3*A)
-            + (A**4 - 8*A**3 + 36*A**2 - 96*A - 480)*np.exp(-4*A)
-            + 120*np.exp(-5*A)
-        )/(1920*A**6*(1 - np.exp(-A))**5)
+        term_eta_8[large] += (
+            -(7*A**4 + 21*A**3 + 105*A**2 + 84*A + 660)*np.exp(-A)
+            +(-77*A**4 - 63*A**3 + 105*A**2 + 252*A + 2640)*np.exp(-2*A)
+            +(-77*A**4 + 63*A**3 + 105*A**2 - 252*A - 3960)*np.exp(-3*A)
+            +(-7*A**4 + 21*A**3 - 105*A**2 + 84*A + 2640)*np.exp(-4*A)
+            -660*np.exp(-5*A)
+        )/(80640 * A**6 * (1 - np.exp(-A))**5)
+
+        term_eta_10[large] += (
+            - (5*A**6 + 25*A**5 + 177*A**4 
+                + 552*A**3 + 2640*A**2 + 43560*A + 199080)*np.exp(-A)
+            + (-285*A**6 - 625*A**5 - 1593*A**4 
+                - 552*A**3 + 7920*A**2 + 217800*A + 1194480)*np.exp(-2*A)
+            - 2*(755*A**6 + 500*A**5 - 885*A**4
+                - 2208*A**3 +2640*A**2 + 217800*A + 1493100)*np.exp(-3*A)
+            - 2*(755*A**6 - 500*A**5 - 885*A**4
+                + 2208*A**3 + 2640*A**2 - 217800*A - 1990800)*np.exp(-4*A)
+            + (-285*A**6 + 625*A**5 - 1593*A**4
+                + 552*A**3 + 7920*A**2 - 217800*A - 2986200)*np.exp(-5*A)
+            + (-5*A**6 + 25*A**5 - 177*A**4
+                + 552*A**3 - 2640*A**2 + 43560*A + 1194480)*np.exp(-6*A)
+            - 199080*np.exp(-7*A)
+        )/(9676800 * A**8 * (1 - np.exp(-A))**7)
 
     if np.any(small):
 
         A = A_all[small]
 
-        term_eta_4[small] = (
-            1/A**3 - 1/(2*A**2) + 1/(12*A) - A/720 + A**3/30240
-            - A**5/1209600
+        term_eta_4[small] += (
+            -1209600/A**3 + 604800/A**2 - 100800/A + 1680*A - 40*A**3 + A**5  
+        )/7257600
+
+        term_eta_6[small] += (
+            -1/(20*A**5) + 7/(480*A**4) - 1/(540*A**3) + 1/(14400*A)
+            - A/226800 + 7*A**3/31104000 - A**5/102643200   
         )
 
-        term_eta_6[small] = (
-            1/(2*A**5) - 1/(8*A**4) + 1/(144*A**3) + A/120960
-            - A**3/1451520 + A**5/27371520   
+        term_eta_8[small] += (
+            -13/(840*A**7) + 11/(2688*A**6) - 1/(1680*A**5) 
+            + 19/(1209600*A**3) - 1/(1270080*A)
+            + 31*A/508032000 + 47*A**3/10059033600
+            + 20039*A**5/66572513280000
         )
 
-        term_eta_8[small] = (
-            3/(16*A**7) - 1/(32*A**6) + 1/(960*A**5) - A/19353600
-            + A**3/109486080 - 691*A**5/830269440000
+        term_eta_10[small] += (
+            -13/(480*A**9) + 79/(7680*A**8) - 3/(2240*A**7)
+            + 281/(24192000*A**5) - 1/(15240960*A**3)
+            - 31*A/80472268800 + 1645271*A**3/26362715258880000
+            - 23*A**5/3736212480000
         )
 
-    prefac = -(1/3 - beta**2/6)
+    prefac = 2*np.sqrt(1 - beta**2)
 
-    if as_pairs:
-
-        return prefac*(
-            term_eta_4*eta**4 + term_eta_6*eta**6 + term_eta_8*eta**8
+    return np.transpose(
+        prefac*np.transpose(
+            eta**4*term_eta_4 + eta**6*term_eta_6
+            + eta**8*term_eta_8 + eta**10*term_eta_10
         )
-
-    else:
-
-        return np.transpose(
-            prefac*np.transpose( 
-                + np.outer(term_eta_4, eta**4)
-                + np.outer(term_eta_6, eta**6)
-                + np.outer(term_eta_8, eta**8)
-            )
-        )
+    )
---
+++ darkhistory/electrons/ics/ics_engloss_spectrum.py
@@ -0,0 +1,261 @@
+"""Nonrelativistic ICS spectrum after integrating over CMB."""
+
+import numpy as np
+
+from darkhistory.electrons.ics.bose_einstein_integrals import *
+from darkhistory.electrons.ics.engloss_diff_terms import *
+from darkhistory import physics as phys
+
+
+from tqdm import tqdm_notebook as tqdm
+
+def engloss_spec_series(eleceng, delta, T, as_pairs=False):
+    """Nonrelativistic ICS energy loss spectrum using the series method. 
+
+    Parameters
+    ----------
+    eleceng : ndarray
+        Incoming electron energy. 
+    delta : ndarray
+        Upscattered photon energy (only positive values). 
+    T : float
+        CMB temperature. 
+    as_pairs : bool
+        If true, treats eleceng and delta as a paired list: produces eleceng.size == photeng.size values. Otherwise, gets the spectrum at each delta for each eleceng, return an array of length eleceng.size*delta.size. 
+
+    Returns
+    -------
+    ndarray
+        dN/(dt d(delta)) of the outgoing photons, with abscissa delta.
+
+    Note
+    ----
+    The final result dN/(dt d(delta)) is the *net* spectrum, i.e. the total number of photons upscattered by delta - number of photons downscattered by delta. 
+
+    """
+
+    print('Computing energy loss spectrum by analytic series...')
+
+    gamma = eleceng/phys.me
+    # Most accurate way of finding beta when beta is small, I think.
+    beta = np.sqrt((eleceng**2/phys.me**2 - 1)/(gamma**2))
+
+    if as_pairs:
+        # neg denotes delta < 0
+        lowlim_down = (1+beta)/(2*beta)*delta/T 
+        lowlim_up = (1-beta)/(2*beta)*delta/T
+    else:
+        lowlim_down = np.outer((1+beta)/(2*beta), delta/T)
+        lowlim_up = np.outer((1-beta)/(2*beta), delta/T)
+
+    prefac = np.float128( 
+        phys.c*(3/8)*phys.thomson_xsec/(2*gamma**3*beta**2)
+        * (8*np.pi/(phys.ele_compton*phys.me)**3)
+    )
+
+    inf_array = np.inf*np.ones_like(lowlim_down)
+
+    print('Computing upscattering loss spectra...')
+
+    print('Computing series 1/7...')
+    F1_up = F1(lowlim_up, inf_array)
+    print('Computing series 2/7...')
+    F0_up = F0(lowlim_up, inf_array)
+    print('Computing series 3/7...')
+    F_inv_up = F_inv(lowlim_up, inf_array)[0]
+    print('Computing series 4/7...')
+    F_x_log_up = F_x_log(lowlim_up, inf_array)[0]
+    print('Computing series 5/7...')
+    F_log_up = F_log(lowlim_up, inf_array)[0]
+    print('Computing series 6/7...')
+    F_x_log_a_up = F_x_log_a(lowlim_up, delta/T)[0]
+    print('Computing series 7/7...')
+    F_log_a_up = F_log_a(lowlim_up, delta/T)[0]
+
+    print('Computing downscattering loss spectra...')
+
+    print('Computing series 1/7...')
+    F1_down = F1(lowlim_down, inf_array)
+    print('Computing series 2/7...')
+    F0_down = F0(lowlim_down, inf_array)
+    print('Computing series 3/7...')
+    F_inv_down = F_inv(lowlim_down, inf_array)[0]
+    print('Computing series 4/7...')
+    F_x_log_down = F_x_log(lowlim_down, inf_array)[0]
+    print('Computing series 5/7...')
+    F_log_down = F_log(lowlim_down, inf_array)[0]
+    print('Computing series 6/7...')
+    F_x_log_a_down = F_x_log_a(lowlim_down, -delta/T)[0]
+    print('Computing series 7/7...')
+    F_log_a_down = F_log_a(lowlim_down, -delta/T)[0]
+
+    term_1_up = np.transpose(
+        (
+            (2/beta)*np.sqrt((1+beta)/(1-beta))
+            + (2/beta)*np.sqrt((1-beta)/(1+beta))
+            + 2/(gamma*beta**2)*np.log((1-beta)/(1+beta))
+        )*np.transpose(T**2*F1_up)
+    )
+
+    term_0_up = np.transpose(
+        (
+            (2/beta)*np.sqrt((1-beta)/(1+beta))
+            - 2*(1-beta)**2/beta**2*np.sqrt((1+beta)/(1-beta))
+            + 2/(gamma*beta**2)*np.log((1-beta)/(1+beta))
+        )*np.transpose(T*delta*F0_up)
+    )
+
+    term_inv_up = np.transpose(
+        -(1-beta)**2/beta**2*np.sqrt((1+beta)/(1-beta))*
+        np.transpose(delta**2*F_inv_up)
+    )
+
+    term_log_up = np.transpose(
+        2/(gamma*beta**2)*np.transpose(
+            -T**2*F_x_log_up - delta*T*F_log_up
+            + T**2*F_x_log_a_up + delta*T*F_log_a_up
+        )
+    )
+
+    term_1_down = np.transpose(
+        (
+            (2/beta)*np.sqrt((1-beta)/(1+beta))
+            + (2/beta)*np.sqrt((1+beta)/(1-beta))
+            - 2/(gamma*beta**2)*np.log((1+beta)/(1-beta))
+        )*np.transpose(T**2*F1_down)
+    )
+
+    term_0_down = -np.transpose(
+        (
+            (2/beta)*np.sqrt((1+beta)/(1-beta))
+            + 2*(1+beta)**2/beta**2*np.sqrt((1-beta)/(1+beta))
+            - 2/(gamma*beta**2)*np.log((1+beta)/(1-beta))
+        )*np.transpose(T*delta*F0_down)
+    )
+
+    term_inv_down = np.transpose(
+        (1+beta)**2/beta**2*np.sqrt((1-beta)/(1+beta))*
+        np.transpose(delta**2*F_inv_down)
+    )
+
+    term_log_down = np.transpose(
+        2/(gamma*beta**2)*np.transpose(
+            T**2*F_x_log_down - delta*T*F_log_down
+            - T**2*F_x_log_a_down + delta*T*F_log_a_down
+        )
+    )
+
+    testing = True
+    if testing:
+        print('***** Diagnostics *****')
+        print('lowlim_up: ', lowlim_up)
+        print('lowlim_down: ', lowlim_down)
+        print('beta: ', beta)
+        print('delta/T: ', delta/T)
+
+        print('***** Individual terms *****')
+        print('term_1_up: ', term_1_up)
+        print('term_0_up: ', term_0_up)
+        print('term_inv_up: ', term_inv_up)
+        print('term_log_up: ', term_log_up)
+        print('term_1_down: ', term_1_down)
+        print('term_0_down: ', term_0_down)
+        print('term_inv_down: ', term_inv_down)
+        print('term_log_down: ', term_log_down)
+
+        print('***** Upscatter and Downscatter Differences*****')
+        print('term_1: ', term_1_up - term_1_down)
+        print('term_0: ', term_0_up - term_0_down)
+        print('term_inv: ', term_inv_up - term_inv_down)
+        print('term_log: ', term_log_up - term_log_down)
+        print('Sum three terms: ', term_0_up - term_0_down
+            + term_inv_up - term_inv_down
+            + term_log_up - term_log_down
+        )
+        print('Sum: ', term_1_up - term_1_down
+            + term_0_up - term_0_down
+            + term_inv_up - term_inv_down
+            + term_log_up - term_log_down)
+
+        print('***** Total Sum (Excluding Prefactor) *****')
+        print('Upscattering loss spectrum: ')
+        print(
+            np.transpose(prefac*np.transpose(
+                term_1_up + term_0_up + term_inv_up + term_log_up
+            )
+        ))
+        print('Downscattering loss spectrum: ')
+        print(
+            np.transpose(prefac*np.transpose(
+                term_1_down + term_0_down + term_inv_down 
+                + term_log_down
+            )
+        ))
+        print('***** End Diagnostics *****')
+
+    return np.transpose(
+        prefac*np.transpose(
+            term_1_up + term_0_up + term_inv_up + term_log_up
+            - term_1_down - term_0_down - term_inv_down
+            - term_log_down
+        )
+    )
+
+def engloss_spec_diff(eleceng, delta, T, as_pairs=False):
+    """Nonrelativistic ICS energy loss spectrum by beta expansion. 
+
+    Parameters
+    ----------
+    eleceng : ndarray
+        Incoming electron energy. 
+    delta : ndarray
+        Energy gained from upscattering by the secondary photon. 
+    T : float
+        CMB temperature. 
+    as_pairs : bool
+        If true, treats eleceng and delta as a paired list: produces eleceng.size == photeng.size values. Otherwise, gets the spectrum at each delta for each eleceng, return an array of length eleceng.size*delta.size. 
+
+    Returns
+    -------
+    ndarray
+        dN/(dt d(delta)) of the outgoing photons, with abscissa delta.
+
+    Note
+    ----
+    The final result dN/(dt d(delta)) is the *net* spectrum, i.e. the total number of photons upscattered by delta - number of photons downscattered by delta. 
+
+    """
+
+    print('Computing energy loss spectrum by beta expansion...')
+
+    gamma = eleceng/phys.me
+    beta = np.sqrt((eleceng**2/phys.me**2 - 1)/(gamma**2))
+
+    prefac = ( 
+        phys.c*(3/8)*phys.thomson_xsec/(2*gamma**3*beta**2)
+        * (8*np.pi/(phys.ele_compton*phys.me)**3)
+        * (T**2/beta**2)
+    )
+
+    print('(1/5) Computing F1_up - F1_down term...')
+    F1_up_down_term = F1_up_down(beta, delta, T, as_pairs=as_pairs)
+    print('(2/5) Computing F0_up - F0_down term...')
+    F0_up_down_diff_term = F0_up_down_diff(beta, delta, T, as_pairs=as_pairs)
+    print('(3/5) Computing F0_up + F0_down term...')
+    F0_up_down_sum_term = F0_up_down_sum(beta, delta, T, as_pairs=as_pairs)
+    print('(4/5) Computing F_inv_up - F_inv_down term...')
+    F_inv_up_down_term = F_inv_up_down(beta, delta, T, as_pairs=as_pairs)
+    print('(5/5) Computing F_rem term...')
+    F_rem_term = F_rem(beta, delta, T, as_pairs=as_pairs)
+
+    term = np.transpose(
+        prefac*np.transpose(
+            F1_up_down_term + F0_up_down_diff_term 
+            + F0_up_down_sum_term
+            + F_inv_up_down_term + F_rem_term
+        )
+    )
+
+    print('Computation by expansion in beta complete!')
+
+    return term
