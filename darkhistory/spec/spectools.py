8f8933143e5927accda005f3ff05e789470ed71f
Hongwan Liu <hongwan@mit.edu>
2017-11-26 19:02:56 -0500

    Speed up of code
    * spectools.bin_boundary: removed np.insert and np.append.
    
    * spectools.rebin_N_arr: out_eng is now optional. When not specified, it is considered equal to in_eng, and the computation is much faster.
    
    * fixed bug associated with spectools.scatter, where out_mode = 'N' was not returning actual N.
    
    * Working example in elec_cooling notebook for resolving photons fully in ICS.

--- darkhistory/spec/spectools.py
+++ darkhistory/spec/spectools.py
@@ -31,12 +31,14 @@ def get_bin_bound(eng):
     log_bin_width_low = np.log(eng[1]) - np.log(eng[0])
     log_bin_width_upp = np.log(eng[-1]) - np.log(eng[-2])
 
-    bin_boundary = np.sqrt(eng[:-1] * eng[1:])
+    bin_boundary = np.zeros(eng.size + 1)
+
+    bin_boundary[1:-1] = np.sqrt(eng[:-1] * eng[1:])
 
     low_lim = np.exp(np.log(eng[0]) - log_bin_width_low / 2)
     upp_lim = np.exp(np.log(eng[-1]) + log_bin_width_upp / 2)
-    bin_boundary = np.insert(bin_boundary, 0, low_lim)
-    bin_boundary = np.append(bin_boundary, upp_lim)
+    bin_boundary[0] = low_lim
+    bin_boundary[-1] = upp_lim
 
     return bin_boundary
 
@@ -51,7 +53,7 @@ def get_log_bin_width(eng):
     bin_boundary = get_bin_bound(eng)
     return np.diff(np.log(bin_boundary))
 
-def rebin_N_arr(N_arr, in_eng, out_eng):
+def rebin_N_arr(N_arr, in_eng, out_eng=None):
     """Rebins an array of particle number with fixed energy.
     
     Returns a `Spectrum` object. The rebinning conserves both total number and total energy.
@@ -62,8 +64,8 @@ def rebin_N_arr(N_arr, in_eng, out_eng):
         An array of number of particles in each bin. 
     in_eng : ndarray
         An array of the energy abscissa for each bin. The total energy in each bin `i` should be `N_arr[i]*in_eng[i]`.
-    out_eng : ndarray
-        The new abscissa to bin into.
+    out_eng : ndarray, optional
+        The new abscissa to bin into. If unspecified, assumed to be in_eng.
 
     Returns
     -------
@@ -94,6 +96,10 @@ def rebin_N_arr(N_arr, in_eng, out_eng):
     if N_arr.size != in_eng.size:
         raise TypeError("The array for number of particles has a different length from the abscissa.")
 
+    if out_eng is None:
+        log_bin_width = get_log_bin_width(in_eng)
+        return Spectrum(in_eng, N_arr/(in_eng*log_bin_width))
+
     if not np.all(np.diff(out_eng) > 0):
         raise TypeError("new abscissa must be ordered in increasing energy.")
     if out_eng[-1] < in_eng[-1]:
@@ -105,17 +111,20 @@ def rebin_N_arr(N_arr, in_eng, out_eng):
     first_bin_eng = np.exp(np.log(out_eng[0]) - (np.log(out_eng[1]) - np.log(out_eng[0])))
     new_eng = np.insert(out_eng, 0, first_bin_eng)
 
-    # Find the relative bin indices for self.eng wrt new_eng. The first bin in new_eng has bin index -1. 
-    bin_ind = np.interp(in_eng, new_eng, 
-        np.arange(new_eng.size)-1, left = -2, right = new_eng.size)
+    # Find the relative bin indices for in_eng wrt new_eng. The first bin in new_eng has bin index -1.
+    
+    bin_ind = np.interp(
+        in_eng, new_eng, np.arange(new_eng.size)-1, 
+        left = -2, right = new_eng.size
+    )
 
     # Locate where bin_ind is below 0, above self.length-1 and in between.
     ind_low = np.where(bin_ind < 0)
     ind_high = np.where(bin_ind == new_eng.size)
     ind_reg = np.where( (bin_ind >= 0) & (bin_ind <= new_eng.size - 1) )
 
-    if ind_high[0].size > 0: 
-        raise OverflowError("the new abscissa lies below the old one: this function cannot handle overflow (yet?).")
+    # if ind_high[0].size > 0: 
+    #     raise OverflowError("the new abscissa lies below the old one: this function cannot handle overflow (yet?).")
 
     # Get the total N and toteng in each bin
     toteng_arr = N_arr*in_eng
@@ -223,14 +232,14 @@ def scatter(
 ):
     """Produces a secondary spectrum. 
 
-    Takes a primary spectrum, and multiplies it with the transfer function. There are two modes: using either a Spectrum object (dN/dE) or with an array of number of particles (N) and an energy abscissa.
+    Takes a primary spectrum, and multiplies it with the transfer function. There are two modes: using either a `Spectrum` object (dN/dE) or with an array of number of particles (N) and an energy abscissa. Similarly, output in the form of a `Spectrum` object (dN/dE) or with an array of number of particles (N) is possible (the energy abscissa is implicitly assumed to be `eng_arr` in this case). 
     
     Parameters
     ----------
     mode : {'dNdE', 'N'}
         Specifies the type of input for the calculation.
     out_mode : {'dNdE', 'N'}
-        Specifies the type of output.
+        Specifies the type of output for the calculation. 
     spec : Spectrum
         The primary spectrum. Required if type is 'dNdE'.
     eng_arr : ndarray
@@ -290,11 +299,18 @@ def scatter(
     # Current fac is disabled because it is too slow.
     # tf *= fac
 
-    if out_mode == 'dNdE':
+    if mode == 'dNdE':
         N_arr = spec.totN('bin')*frac
+    elif mode == 'N':
+        N_arr *= frac
+
+    if out_mode == 'dNdE':
         return tf.sum_specs(N_arr)
     elif out_mode == 'N':
-        return np.dot(N_arr, tf.get_grid_values())
+        return (
+            np.dot(N_arr, tf.get_grid_values())
+            *new_eng*get_log_bin_width(new_eng)
+        )
 
 def evolve(spec, tflist, end_rs=None, save_steps=False):
     """Evolves a spectrum using a list of transfer functions. 
