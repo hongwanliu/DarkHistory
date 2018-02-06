58291f68bbb15a8235f20cdc0ae4f7888048a759
Hongwan Liu <hongwan@mit.edu>
2018-02-06 18:34:34 -0500

    ICS cooling file name strings

--- darkhistory/electrons/ics/ics_cooling.py
+++ darkhistory/electrons/ics/ics_cooling.py
@@ -5,14 +5,30 @@ import pickle
 
 import darkhistory.physics as phys
 
-def get_ICS_photon_spectrum:
+from darkhistory.electrons.ics.ics_spectrum import ics_spec 
+from darkhistory.electrons.ics.ics_engloss_spectrum import engloss_spectrum 
+
+def get_ICS_photon_spectrum(
+    nonrel_tf_filename, rel_tf_filename, engloss_tf_filename
+):
     """Returns the photon spectrum obtained by electron cooling through ICS. 
 
     Parameters
     ----------
+    nonrel_tf_filename : string
+        The file name for the nonrelativistic ICS photon spectrum produced by a single primary electron scattering.
+    rel_tf_filename : string
+        The file name for the relativistic ICS photon spectrum produced by a single primary electron scattering. 
+    engloss_tf_filename : string
+        The file name for energy loss ICS spectrum produced by a single primary electron scattering.  
 
     Returns
     -------
 
     """
 
+
+
+    raw_nonrel_ICS_tf = pickle.load(open("/Users/hongwan/Dropbox (MIT)/Photon Deposition/ICS_nonrel.raw","rb"))
+    raw_rel_ICS_tf = pickle.load(open("/Users/hongwan/Dropbox (MIT)/Photon Deposition/ICS_rel.raw","rb"))
+    raw_engloss_tf = pickle.load(open("/Users/hongwan/Dropbox (MIT)/Photon Deposition/ICS_englossspec.raw","rb"))
