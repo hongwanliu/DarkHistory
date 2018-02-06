45a76b87dd2cbc3b9c4eb21c319c4f268517f65d
Hongwan Liu <hongwan@mit.edu>
2018-02-06 18:30:44 -0500

    New file for main ICS function

---
+++ darkhistory/electrons/ics/ics_cooling.py
@@ -0,0 +1,18 @@
+"""Electron cooling through ICS."""
+
+import numpy as np 
+import pickle
+
+import darkhistory.physics as phys
+
+def get_ICS_photon_spectrum:
+    """Returns the photon spectrum obtained by electron cooling through ICS. 
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+
