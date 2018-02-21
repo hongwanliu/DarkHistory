fb71cb303d5ebce047964b50c8486dedb9cd2a6b
Gregory Ridgway <gridgway@mit.edu>
2018-02-20 21:15:41 -0500

    lowE_photons.py also computes f(z) for excitation of HI

--- darkhistory/low_energy/lowE_photons.py
+++ darkhistory/low_energy/lowE_photons.py
@@ -4,29 +4,34 @@ sys.path.append("../..")
 import numpy as np
 import physics as phys
 
-def compute_dep_inj_ionization_ratio(photon_spectrum, n, tot_inj):
-    """ Given a photon spectrum object, deposit its energy into HI, HeI, and HeII ionization in that order
+def compute_dep_inj_ionization_ratio(photon_spectrum, n, tot_inj, time_step):
+    """ Given a spectrum of photons, deposit its energy into HI excitation and HI, HeI, HeII ionization in that order.  The
+        spectrum must provide the number density of photons within each bin.
         Q: can photons heat the IGM?  Should this method keep track of the fact that x_e, xHII, etc. are changing?
 
     Parameters
     ----------
     photon_spectrum : Spectrum object
         spectrum of photons
-    n : (float,float,float)
+    n : list of floats
         density of (HI, HeI, HeII).
     tot_inj : float
         total energy injected by DM
+    time_step : float
+        amount of time during which energy deposition takes place
 
     Returns
     -------
-    list of floats
+    tuple of floats
         Ratio of deposited energy to a given channel over energy deposited by DM.
-        The order of the channels is HI, HeI, and HeII ionization
+        The order of the channels is HI excitation and HI, HeI, HeII ionization
     """
 
+    # Assume 100% of photons within window [10.2eV, 13.6eV] are absorbed
+    excite_HI = photon_spectrum.totN(bound_type='eng', bound_arr=np.array([phys.lya_eng,phys.rydberg]))[0]/(tot_inj*time_step)
     # [# photons absorbed] / [unit time] = N(E) \sigma(E) n_species c
     ionHI = sum(photon_spectrum.N*phys.photo_ion_xsec(photon_spectrum.eng,'H0'))*n[0]*phys.c/tot_inj
     ionHeI = sum(photon_spectrum.N*phys.photo_ion_xsec(photon_spectrum.eng,'He0'))*n[1]*phys.c/tot_inj
     ionHeII = sum(photon_spectrum.N*phys.photo_ion_xsec(photon_spectrum.eng,'He1'))*n[2]*phys.c/tot_inj
 
-    return ionHI, ionHeI, ionHeII
+    return excite_HI, ionHI, ionHeI, ionHeII
