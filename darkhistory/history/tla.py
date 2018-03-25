e60ba9872efd7bdce7632e604802b4b5af4d4246
Gregory Ridgway <gridgway@mit.edu>
2018-03-24 22:26:50 -0400

    Recovered old tla.py, added old MEDEA data files

--- darkhistory/history/tla.py
+++ darkhistory/history/tla.py
@@ -41,96 +41,6 @@ def compton_cooling_rate(xHII, xHeII, xHeIII, T_m, rs):
 		* phys.TCMB(rs)**4
 	)
 
-def alpha_recomb(T_matter):
-	"""Case-B recombination coefficient.
-
-	Parameters
-	----------
-	T_matter : float
-		The matter temperature.
-
-	Returns
-	-------
-	float
-		Case-B recombination coefficient in cm^3/s.
-	"""
-
-	# Fudge factor recommended in 1011.3758
-	fudge_fac = 1.126
-
-	return (
-		fudge_fac * 1e-13 * 4.309 * (1.16405*T_matter)**(-0.6166)
-		/ (1 + 0.6703 * (1.16405*T_matter)**0.5300)
-	)
-
-def beta_ion(T_rad):
-	"""Case-B photoionization coefficient.
-
-	Parameters
-	----------
-	T_rad : float
-		The radiation temperature.
-
-	Returns
-	-------
-	float
-		Case-B photoionization coefficient in s^-1.
-
-	"""
-	reduced_mass = phys.mp*phys.me/(phys.mp + phys.me)
-	de_broglie_wavelength = (
-		phys.c * 2*np.pi*phys.hbar
-		/ np.sqrt(2 * np.pi * reduced_mass * T_rad)
-	)
-	return (
-		(1/de_broglie_wavelength)**3/4
-		* np.exp(-phys.rydberg/4/T_rad) * alpha_recomb(T_rad)
-	)
-
-
-# def betae(Tr):
-# 	# Case-B photoionization coefficient
-# 	thermlambda = c*(2*pi*hbar)/sqrt(2*pi*(mp*me/(me+mp))*Tr)
-# 	return alphae(Tr) * exp(-(rydberg/4)/Tr)/(thermlambda**3)
-
-def peebles_C(xe, rs):
-	"""Returns the Peebles C coefficient.
-
-	This is the ratio of the total rate for transitions from n = 2 to the ground state to the total rate of all transitions, including ionization.
-
-	Parameters
-	----------
-	xe : float
-		The ionization fraction ne/nH.
-	Tm : float
-		The matter temperature.
-	rs : float
-		The redshift in 1+z.
-
-	Returns
-	-------
-	float
-		The Peebles C factor.
-	"""
-
-	# Net rate for 2p to 1s transition.
-	rate_2p1s = (
-		8 * np.pi * phys.hubble(rs)
-		/(3*(phys.nH*rs**3 * (1-xe) * (phys.c/phys.lya_freq)**3))
-	)
-
-	# Net rate for 2s to 1s transition.
-	rate_2s1s = phys.width_2s1s
-
-	# Net rate for ionization.
-	rate_ion = beta_ion(phys.TCMB(rs))
-
-	# Rate is averaged over 3/4 of excited state being in 2p, 1/4 in 2s.
-	return (
-		(3*rate_2p1s/4 + rate_2s1s/4)
-		/(3*rate_2p1s/4 + rate_2s1s/4 + rate_ion)
-	)
-
 def get_history(
 	init_cond, f_H_ion, f_H_exc, f_heating,
 	dm_injection_rate, rs_vec, reion_switch=True
@@ -228,15 +138,15 @@ def get_history(
 
 			return 2 * np.cosh(yHII)**2 * -phys.dtdz(rs) * (
 				# Recombination processes
-				- peebles_C(xHII(yHII), rs) * (
-					alpha_recomb(T_m) * xHII(yHII)*xe * phys.nH * rs**3
-					- beta_ion(phys.TCMB(rs)) * xHI
+				- phys.peebles_C(xHII(yHII), rs) * (
+					phys.alpha_recomb(T_m) * xHII(yHII)*xe * phys.nH * rs**3
+					- phys.beta_ion(phys.TCMB(rs)) * xHI
 						* np.exp(-phys.lya_eng/T_m)
 				)
 				# DM injection. Note that C = 1 at late times.
 				+ f_H_ion(rs, xHI, xHeI, xHeII(yHeII)) * dm_injection_rate(rs)
 					/ (phys.rydberg * phys.nH * rs**3)
-				+ (1 - peebles_C(xHII(yHII), rs)) * (
+				+ (1 - phys.peebles_C(xHII(yHII), rs)) * (
 					f_H_exc(rs, xHI, xHeI, xHeII(yHeII)) * dm_injection_rate(rs)
 					/ (phys.lya_eng * phys.nH * rs**3)
 				)
@@ -345,7 +255,7 @@ def get_history(
 				# DM injection. Note that C = 1 at late times.
 				+ f_H_ion(rs, xHI, xHeI, xHeII(yHeII)) * dm_injection_rate(rs)
 					/ (phys.rydberg * phys.nH * rs**3)
-				+ (1 - peebles_C(xHII(yHII), rs)) * (
+				+ (1 - phys.peebles_C(xHII(yHII), rs)) * (
 					f_H_exc(rs, xHI, xHeI, xHeII(yHeII)) * dm_injection_rate(rs)
 					/ (phys.lya_eng * phys.nH * rs**3)
 				)
