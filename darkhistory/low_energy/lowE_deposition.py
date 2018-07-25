38699877f890d12c9919792c7a32de3f50916b2a
Gregory Ridgway <gridgway@mit.edu>
2018-07-25 17:48:15 -0400

    Example10: Successfully replaced block of code in Greg's Checks section by lowE_deposition.compute_fs()

--- darkhistory/low_energy/lowE_deposition.py
+++ darkhistory/low_energy/lowE_deposition.py
@@ -12,21 +12,21 @@ from darkhistory.low_energy import lowE_photons
 def compute_fs(spec_elec, spec_phot, x, dE_dVdt, time_step, method="old"):
     """ Compute f(z) fractions for continuum photons, photoexcitation of HI, and photoionization of HI, HeI, HeII
 
-    Given a spectrum of deposited photons, resolve its energy into continuum photons,
+    Given a spectrum of deposited electrons and photons, resolve their energy into continuum photons,
     HI excitation, and HI, HeI, HeII ionization in that order.
 
     Parameters
      ----------
     spec_phot : Spectrum object
-        spectrum of photons. Assumed to be in dNdE mode. spec.toteng() should return energy _per baryon_ per time.
+        spectrum of photons. Assumed to be in dNdE mode. spec.totN() should return number _per baryon_ per time.
     spec_elec : Spectrum object
-        spectrum of electrons. Assumed to be in dNdE mode. spec.toteng() should return energy _per baryon_ per time.
+        spectrum of electrons. Assumed to be in dNdE mode. spec.totN() should return number _per baryon_ per time.
     x : list of floats
         number of (HI, HeI, HeII) divided by nH at redshift photon_spectrum.rs
     dE_dVdt : float
         DM energy injection rate, dE/dVdt injected.
     time_step : float
-        The time-step associated with the deposited spectra.
+        The time-step associated with the deposited spectra, in seconds.
     method : {'old','ion','new'}
         'old': All photons >= 13.6eV ionize hydrogen, within [10.2, 13.6)eV excite hydrogen, < 10.2eV are labelled continuum.
         'ion': Same as 'old', but now photons >= 13.6 can ionize HeI and HeII also.
@@ -44,28 +44,28 @@ def compute_fs(spec_elec, spec_phot, x, dE_dVdt, time_step, method="old"):
     Think about the exceptions that should be thrown (spec_elec.rs should equal spec_phot.rs)
     """
     ion_indx = spectools.get_indx(
-        eng, phys.rydberg
+        spec_phot.eng, phys.rydberg
     ) #Check this, also check spec_phot.eng?
 
     ionized_elec = Spectrum(
         spec_phot.eng[ion_indx:], spec_phot.N[ion_indx:], rs=spec_phot.rs, spec_type='N'
     ) #Change this so that it uses totN(bin_bounds)
 
-    new_eng = tmp_elec.eng - phys.rydberg
+    new_eng = ionized_elec.eng - phys.rydberg
     if new_eng[0] < 0: #Is this the best way to do this?
         new_eng = np.insert(new_eng[1:], 0, 1e-12)
     ionized_elec.shift_eng(new_eng)
 
     # rebin so that ionized_elec may be added to spec_elec
-    indx = tmp_elec.eng.size
-    ionized_elec.rebin(loweng_elec.eng[:indx+1])
-    loweng_elec.N[:indx+1] += ionized_elec.N
+    indx = ionized_elec.eng.size
+    ionized_elec.rebin(spec_elec.eng[:indx+1])
+    spec_elec.N[:indx+1] += ionized_elec.N
 
     f_phot = lowE_photons.compute_fs(
         spec_phot, x, dE_dVdt, time_step, method
     )
     f_elec = lowE_electrons.compute_fs(
-        elec_phot, 1-x[0], dE_dVdt, time_step
+        spec_elec, 1-x[0], dE_dVdt, time_step
     )
 
     return f_phot + f_elec
