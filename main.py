34b8e7a760f09c696888485c6cba287f684f3709
Gregory Ridgway <gridgway@mit.edu>
2018-11-26 18:15:50 -0500

    fixed the tqdm bar to account for coarsening

--- main.py
+++ main.py
@@ -330,7 +330,7 @@ def evolve(
     # tqdm related stuff. 
     if use_tqdm:
         from tqdm import tqdm_notebook as tqdm
-        pbar = tqdm(total=np.floor((np.log(rs) - np.log(end_rs))/dlnz))
+        pbar = tqdm(total=np.ceil((np.log(rs) - np.log(end_rs))/dlnz/coarsen_factor))
 
     # Function that changes the normalization 
     # from per annihilation to per baryon in the step.
@@ -449,7 +449,6 @@ def evolve(
             deposited_heat/dt,
             deposited_ICS/dt
         ]])
-        
 
     MEDEA_interp = make_interpolator()
 
