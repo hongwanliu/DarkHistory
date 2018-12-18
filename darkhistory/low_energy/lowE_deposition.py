22fe9e75af4054b101d78ae90160be91c6781fab
Gregory Ridgway <gridgway@mit.edu>
2018-12-18 15:05:56 -0500

    getting rid of f_elec_continuum for comparison's sake

--- darkhistory/low_energy/lowE_deposition.py
+++ darkhistory/low_energy/lowE_deposition.py
@@ -88,7 +88,7 @@ def compute_fs(MEDEA_interp, elec_spec, phot_spec, x, dE_dVdt_inj, dt, highengde
         f_phot[3]+f_phot[4]+f_elec[3],
         f_phot[1]+f_elec[1],
         f_elec[4],
-        f_phot[0]+f_elec[0] - cmbloss*phys.nB*phot_spec.rs**3 / dE_dVdt_inj
+        f_phot[0]+f_elec[0]*0 - cmbloss*phys.nB*phot_spec.rs**3 / dE_dVdt_inj
     ])
 
     f_high = np.array([
