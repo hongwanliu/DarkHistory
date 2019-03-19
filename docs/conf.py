cc4c155f950ec5b518f6ccffa2279222c60dfb4b
Hongwan Liu <hongwan@mit.edu>
2019-03-19 16:04:16 -0400

    conf.py update #4

--- docs/conf.py
+++ docs/conf.py
@@ -96,12 +96,12 @@ def setup(app):
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary',
-#     'sphinx.ext.doctest','sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.autosummary',
+    'sphinx.ext.doctest','sphinx.ext.napoleon']
 
 # extensions = ['numpydoc']
 
-numpydoc_class_members_toctree = False
+# numpydoc_class_members_toctree = False
 
 # Napoleon settings
 
