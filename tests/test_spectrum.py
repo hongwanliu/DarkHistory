02fc99cd58a017e1260d19df87ec85a3d0c75e0e
Hongwan Liu <hongwan@mit.edu>
2019-03-29 18:09:21 -0400

    Uncommented tf_data, normalize_whitespace added to pytest.ini

--- tests/test_spectrum.py
+++ tests/test_spectrum.py
@@ -4,15 +4,6 @@ from pytest import approx
 
 from darkhistory.spec.spectrum import Spectrum
 
-def test_totN():
-
-    eng = np.array([1, 10, 100, 1000])
-    N   = np.array([1, 2, 3, 4])
-    spec = Spectrum(eng, N, spec_type='N')
-    assert spec.totN() == 10.0
-    assert spec.totN('bin', np.array([1, 3])) == np.array([5.])
-    assert spec.totN('eng', np.array([10, 1e4])) == np.array([8.])
-
 def test_redshift():
 
     eng = np.array([3, 10, 29, 3000])
