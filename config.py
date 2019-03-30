f2286284bb6fdfd59bccf2dfc2b334754e0cdb46
Hongwan Liu <hongwan@mit.edu>
2019-03-30 11:58:17 -0400

    Completed loading PPPC data from config
    * PChipInterpolator2D moved to config
    * load_data now takes 'pppc' and primary='e', for example, and it only returns the 2D interpolator for elec and phot in that channel.
    * get_data added to get_pppc_spec

--- config.py
+++ config.py
@@ -11,24 +11,162 @@ import pickle
 
 data_path = '/Users/hongwan/Dropbox (MIT)/Photon Deposition/DarkHistory_data'
 
-def load_data(data_type):
+class PchipInterpolator2D: 
+
+    """ 2D interpolation over PPPC4DMID raw data, using PCHIP method.
+
+    Parameters
+    -----------
+    pri : string
+        Specifies primary annihilation channel. See below for list.
+    sec : {'elec', 'phot'}
+        Specifies stable SM secondary to get the spectrum of.
+
+    Attributes
+    ----------
+    pri : string
+        Specifies primary annihilation channel. See below for list.
+    sec : {'elec', 'phot'}
+        Specifies stable SM secondary to get the spectrum of.
+    get_val : function
+        Returns the interpolation value at (mDM_in_GeV, log10(K/mDM)) where K is the kinetic energy of the secondary.
+
+    """
+    
+    def __init__(self, pri, sec):
+        if sec == 'elec':
+            i = 0
+            # fac is used to multiply the raw electron data by 2 to get the
+            # e+e- spectrum that we always use in DarkHistory.
+            fac = 2.
+        elif sec == 'phot':
+            i = 1
+            fac = 1.
+        else:
+            raise TypeError('invalid final state.')
+            
+        self.pri = pri
+        self.sec = sec
+
+        # To compute the spectrum of 'e', we average over 'e_L' and 'e_R'.
+        # We do the same thing for 'mu', 'tau', 'W' and 'Z'.
+        # To avoid thinking too much, all spectra are split into two parts.
+        # self._weight gives the weight of each half.
+            
+        if pri == 'e' or pri == 'mu' or pri == 'tau':
+            pri_1 = pri + '_L'
+            pri_2 = pri + '_R'
+            self._weight = [0.5, 0.5]
+        elif pri == 'W' or pri == 'Z':
+            # 2 transverse pol., 1 longitudinal.
+            pri_1 = pri + '_T'
+            pri_2 = pri + '_L'
+            self._weight = [2/3, 1/3]
+        else:
+            pri_1 = pri
+            pri_2 = pri
+            self._weight = [0.5, 0.5]
+
+        idx_list_data = {
+            'e_L': 0, 'e_R': 1, 'mu_L': 2, 'mu_R': 3, 'tau_L': 4, 'tau_R': 5,
+            'q': 6, 'c': 7, 'b': 8, 't': 9,
+            'W_L': 10, 'W_T': 11, 'Z_L': 12, 'Z_T': 13, 
+            'g': 14, 'gamma': 15, 'h': 16,
+            'nu_e': 17, 'nu_mu': 18, 'nu_tau': 19,
+            'VV_to_4e': 20, 'VV_to_4mu': 21, 'VV_to_4tau': 22
+        } 
+        
+        # Compile the raw data.
+        mDM_in_GeV_arr_1 = np.array(
+            coords_data[i, idx_list_data[pri_1], 0]
+        )
+        log10x_arr_1     = np.array(
+            coords_data[i, idx_list_data[pri_1], 1]
+        )
+        values_arr_1     = np.array(values_data[i, idx_list_data[pri_1]])
+
+        mDM_in_GeV_arr_2 = np.array(
+            coords_data[i, idx_list_data[pri_2], 0]
+        )
+        log10x_arr_2     = np.array(
+            coords_data[i, idx_list_data[pri_2], 1]
+        )
+        values_arr_2     = np.array(values_data[i, idx_list_data[pri_2]])
+
+        self._mDM_in_GeV_arrs = [mDM_in_GeV_arr_1, mDM_in_GeV_arr_2] 
+        self._log10x_arrs     = [log10x_arr_1,     log10x_arr_2]
+
+        # Save the 1D PCHIP interpolator over mDM_in_GeV. Multiply the 
+        # electron spectrum by 2 by adding np.log10(2).  
+        self._interpolators = [
+            PchipInterpolator(
+                mDM_in_GeV_arr_1, values_arr_1 + np.log10(fac), 
+                extrapolate=False
+            ),
+            PchipInterpolator(
+                mDM_in_GeV_arr_2, values_arr_2 + np.log10(fac),
+                extrapolate=False
+            )
+        ]
+    
+    def get_val(self, mDM_in_GeV, log10x):
+        
+        if (
+            mDM_in_GeV < self._mDM_in_GeV_arrs[0][0] 
+            or mDM_in_GeV < self._mDM_in_GeV_arrs[1][0]
+            or mDM_in_GeV > self._mDM_in_GeV_arrs[0][-1]
+            or mDM_in_GeV > self._mDM_in_GeV_arrs[1][-1]
+        ):
+            raise TypeError('mDM lies outside of the interpolation range.')
+        
+        # Call the saved interpolator at mDM_in_GeV, 
+        # then use PCHIP 1D interpolation at log10x. 
+        result1 = pchip_interpolate(
+            self._log10x_arrs[0], self._interpolators[0](mDM_in_GeV), log10x
+        )
+        # Set all values outside of the log10x interpolation range to 
+        # (effectively) zero. 
+        result1[log10x >= self._log10x_arrs[0][-1]] = -100.
+        result1[log10x <= self._log10x_arrs[0][0]]  = -100.
+        
+        result2 = pchip_interpolate(
+            self._log10x_arrs[1], self._interpolators[1](mDM_in_GeV), log10x
+        )
+        result2[log10x >= self._log10x_arrs[1][-1]] = -100.
+        result2[log10x <= self._log10x_arrs[1][0]]  = -100.
+        
+        # Combine the two spectra.  
+        return np.log10(
+            self._weight[0]*10**result1 + self._weight[1]*10**result2
+        )
+
+def load_data(data_type, primary=None):
     """ Loads data from downloaded files. 
 
     Parameters
     ----------
-    data_type:{'binning', 'dep_tf', 'ics_tf', 'struct', 'hist', 'pppc'}
+    data_type : {'binning', 'dep_tf', 'ics_tf', 'struct', 'hist', 'pppc'}
         Type of data to load. The options are: 
         * *'binning'*: Default binning for all transfer functions; 
         * *'dep_tf'*: Transfer functions for propagating photons and deposition into low-energy photons, low-energy electrons, high-energy deposition and upscattered CMB energy rate;
         * *'ics_tf'*: Transfer functions for ICS for scattered photons in the Thomson regime, relativistic regime, and scattered electron energy-loss spectrum; 
         * *'struct'*: Structure formation boosts; 
         * *'hist'*: Baseline ionization and temperature histories, and
-        * *'pppc'*: Data from PPPC4DMID for annihilation spectra. 
+        * *'pppc'*: Data from PPPC4DMID for annihilation spectra. Specify the primary channel in *primary*. 
+
+    primary : str, optional
+        The valid options are *{'e_L','e_R', 'e', 'mu_L', 'mu_R', 'mu', 'tau_L', 'tau_R', 'tau', 'q',  'c',  'b', 't', 'W_L', 'W_T', 'W', 'Z_L', 'Z_T', 'Z', 'g',  'gamma', 'h', 'nu_e', 'nu_mu', 'nu_tau', 'VV_to_4e', 'VV_to_4mu', 'VV_to_4tau'}*. See :func:`.get_pppc_spec` for more details.
+
 
     Returns
     --------
     dict
         A dictionary of the data requested. 
+
+    See Also
+    ---------
+    :func:`.get_pppc_spec`
+
     """
 
     if data_path == '' or not os.path.isdir(data_path):
@@ -153,6 +291,17 @@ def load_data(data_type):
         with open(values_file_name) as data_file:
             values_data = np.array(json.load(data_file))
 
+        # Dictionary for the interpolators
+        dlNdlxIEW_interp = {}
+
+        dlNdlxIEW_interp['elec'] = PchipInterpolator2D(pri, 'elec')
+        dlNdlxIEW_interp['phot'] = PchipInterpolator2D(pri, 'phot')
+
+        return dlNdlxIEW_interp
+
+    else:
+
+        raise ValueError('invalid data_type.')
 
 
 # # Location of all data files. 
--- darkhistory/spec/pppc.py
+++ darkhistory/spec/pppc.py
@@ -9,7 +9,8 @@ import json
 from scipy.interpolate import PchipInterpolator
 from scipy.interpolate import pchip_interpolate
 
-from config import data_path
+# from config import data_path
+from config import get_data
 
 import darkhistory.physics as phys
 from darkhistory.spec.spectrum import Spectrum
@@ -18,19 +19,19 @@ from darkhistory.spec.spectools import rebin_N_arr
 # PPPC data.  
 
 # Import data. 
-coords_file_name = (
-    data_path+'/dlNdlxIEW_coords_table.txt'
-)
-values_file_name = (
-    data_path+'/dlNdlxIEW_values_table.txt'
-)
+# coords_file_name = (
+#     data_path+'/dlNdlxIEW_coords_table.txt'
+# )
+# values_file_name = (
+#     data_path+'/dlNdlxIEW_values_table.txt'
+# )
 
-if 'pytest' not in sys.modules and 'readthedocs' not in sys.modules: 
+# if 'pytest' not in sys.modules and 'readthedocs' not in sys.modules: 
 
-    with open(coords_file_name) as data_file:    
-        coords_data = np.array(json.load(data_file))
-    with open(values_file_name) as data_file:
-        values_data = np.array(json.load(data_file))
+#     with open(coords_file_name) as data_file:    
+#         coords_data = np.array(json.load(data_file))
+#     with open(values_file_name) as data_file:
+#         values_data = np.array(json.load(data_file))
 
 
 # coords_data is a (2, 23, 2) array. 
@@ -46,152 +47,144 @@ if 'pytest' not in sys.modules and 'readthedocs' not in sys.modules:
 # Each element is a 2D array, indexed by {mDM in GeV, np.log10(K/mDM)}
 # as saved in coords_data. 
 
-# chan_list_data = [
-#     'e_L', 'e_R', 'mu_L', 'mu_R', 'tau_L', 'tau_R',
+
+# class PchipInterpolator2D: 
+
+#     """ 2D interpolation over the raw data, using PCHIP method.
+
+#     Parameters
+#     -----------
+#     pri : string
+#         Specifies primary annihilation channel. See below for list.
+#     sec : {'elec', 'phot'}
+#         Specifies stable SM secondary to get the spectrum of.
+
+#     Attributes
+#     ----------
+#     pri : string
+#         Specifies primary annihilation channel. See below for list.
+#     sec : {'elec', 'phot'}
+#         Specifies stable SM secondary to get the spectrum of.
+#     get_val : function
+#         Returns the interpolation value at (mDM_in_GeV, log10(K/mDM)) where K is the kinetic energy of the secondary.
+
+#     """
+    
+#     def __init__(self, pri, sec):
+#         if sec == 'elec':
+#             i = 0
+#             # fac is used to multiply the raw electron data by 2 to get the
+#             # e+e- spectrum that we always use in DarkHistory.
+#             fac = 2.
+#         elif sec == 'phot':
+#             i = 1
+#             fac = 1.
+#         else:
+#             raise TypeError('invalid final state.')
+            
+#         self.pri = pri
+#         self.sec = sec
+
+#         # To compute the spectrum of 'e', we average over 'e_L' and 'e_R'.
+#         # We do the same thing for 'mu', 'tau', 'W' and 'Z'.
+#         # To avoid thinking too much, all spectra are split into two parts.
+#         # self._weight gives the weight of each half.
+            
+#         if pri == 'e' or pri == 'mu' or pri == 'tau':
+#             pri_1 = pri + '_L'
+#             pri_2 = pri + '_R'
+#             self._weight = [0.5, 0.5]
+#         elif pri == 'W' or pri == 'Z':
+#             # 2 transverse pol., 1 longitudinal.
+#             pri_1 = pri + '_T'
+#             pri_2 = pri + '_L'
+#             self._weight = [2/3, 1/3]
+#         else:
+#             pri_1 = pri
+#             pri_2 = pri
+#             self._weight = [0.5, 0.5]
+
+#         idx_list_data = {
+#             'e_L': 0, 'e_R': 1, 'mu_L': 2, 'mu_R': 3, 'tau_L': 4, 'tau_R': 5,
+#             'q': 6, 'c': 7, 'b': 8, 't': 9,
+#             'W_L': 10, 'W_T': 11, 'Z_L': 12, 'Z_T': 13, 
+#             'g': 14, 'gamma': 15, 'h': 16,
+#             'nu_e': 17, 'nu_mu': 18, 'nu_tau': 19,
+#             'VV_to_4e': 20, 'VV_to_4mu': 21, 'VV_to_4tau': 22
+#         } 
+        
+#         # Compile the raw data.
+#         mDM_in_GeV_arr_1 = np.array(
+#             coords_data[i, idx_list_data[pri_1], 0]
+#         )
+#         log10x_arr_1     = np.array(
+#             coords_data[i, idx_list_data[pri_1], 1]
+#         )
+#         values_arr_1     = np.array(values_data[i, idx_list_data[pri_1]])
+
+#         mDM_in_GeV_arr_2 = np.array(
+#             coords_data[i, idx_list_data[pri_2], 0]
+#         )
+#         log10x_arr_2     = np.array(
+#             coords_data[i, idx_list_data[pri_2], 1]
+#         )
+#         values_arr_2     = np.array(values_data[i, idx_list_data[pri_2]])
+
+#         self._mDM_in_GeV_arrs = [mDM_in_GeV_arr_1, mDM_in_GeV_arr_2] 
+#         self._log10x_arrs     = [log10x_arr_1,     log10x_arr_2]
+
+#         # Save the 1D PCHIP interpolator over mDM_in_GeV. Multiply the 
+#         # electron spectrum by 2 by adding np.log10(2).  
+#         self._interpolators = [
+#             PchipInterpolator(
+#                 mDM_in_GeV_arr_1, values_arr_1 + np.log10(fac), 
+#                 extrapolate=False
+#             ),
+#             PchipInterpolator(
+#                 mDM_in_GeV_arr_2, values_arr_2 + np.log10(fac),
+#                 extrapolate=False
+#             )
+#         ]
+    
+#     def get_val(self, mDM_in_GeV, log10x):
+        
+#         if (
+#             mDM_in_GeV < self._mDM_in_GeV_arrs[0][0] 
+#             or mDM_in_GeV < self._mDM_in_GeV_arrs[1][0]
+#             or mDM_in_GeV > self._mDM_in_GeV_arrs[0][-1]
+#             or mDM_in_GeV > self._mDM_in_GeV_arrs[1][-1]
+#         ):
+#             raise TypeError('mDM lies outside of the interpolation range.')
+        
+#         # Call the saved interpolator at mDM_in_GeV, 
+#         # then use PCHIP 1D interpolation at log10x. 
+#         result1 = pchip_interpolate(
+#             self._log10x_arrs[0], self._interpolators[0](mDM_in_GeV), log10x
+#         )
+#         # Set all values outside of the log10x interpolation range to 
+#         # (effectively) zero. 
+#         result1[log10x >= self._log10x_arrs[0][-1]] = -100.
+#         result1[log10x <= self._log10x_arrs[0][0]]  = -100.
+        
+#         result2 = pchip_interpolate(
+#             self._log10x_arrs[1], self._interpolators[1](mDM_in_GeV), log10x
+#         )
+#         result2[log10x >= self._log10x_arrs[1][-1]] = -100.
+#         result2[log10x <= self._log10x_arrs[1][0]]  = -100.
+        
+#         # Combine the two spectra.  
+#         return np.log10(
+#             self._weight[0]*10**result1 + self._weight[1]*10**result2
+#         )
+
+# This list includes 'e', 'mu', 'tau', 'W' and 'Z'. 
+# chan_list = [
+#     'e_L','e_R', 'e', 'mu_L', 'mu_R', 'mu', 'tau_L', 'tau_R', 'tau',
 #     'q',  'c',  'b', 't',
-#     'W_L', 'W_T', 'Z_L', 'Z_T', 
-#     'g',  'gamma', 'h',
+#     'W_L', 'W_T', 'W', 'Z_L', 'Z_T', 'Z', 'g',  'gamma', 'h',
 #     'nu_e', 'nu_mu', 'nu_tau',
 #     'VV_to_4e', 'VV_to_4mu', 'VV_to_4tau'
 # ]
-
-idx_list_data = {
-    'e_L': 0, 'e_R': 1, 'mu_L': 2, 'mu_R': 3, 'tau_L': 4, 'tau_R': 5,
-    'q': 6, 'c': 7, 'b': 8, 't': 9,
-    'W_L': 10, 'W_T': 11, 'Z_L': 12, 'Z_T': 13, 
-    'g': 14, 'gamma': 15, 'h': 16,
-    'nu_e': 17, 'nu_mu': 18, 'nu_tau': 19,
-    'VV_to_4e': 20, 'VV_to_4mu': 21, 'VV_to_4tau': 22
-} 
-
-class PchipInterpolator2D: 
-
-    """ 2D interpolation over the raw data, using PCHIP method.
-
-    Parameters
-    -----------
-    pri : string
-        Specifies primary annihilation channel. See below for list.
-    sec : {'elec', 'phot'}
-        Specifies stable SM secondary to get the spectrum of.
-
-    Attributes
-    ----------
-    pri : string
-        Specifies primary annihilation channel. See below for list.
-    sec : {'elec', 'phot'}
-        Specifies stable SM secondary to get the spectrum of.
-    get_val : function
-        Returns the interpolation value at (mDM_in_GeV, log10(K/mDM)) where K is the kinetic energy of the secondary.
-
-    """
-    
-    def __init__(self, pri, sec):
-        if sec == 'elec':
-            i = 0
-            # fac is used to multiply the raw electron data by 2 to get the
-            # e+e- spectrum that we always use in DarkHistory.
-            fac = 2.
-        elif sec == 'phot':
-            i = 1
-            fac = 1.
-        else:
-            raise TypeError('invalid final state.')
-            
-        self.pri = pri
-        self.sec = sec
-
-        # To compute the spectrum of 'e', we average over 'e_L' and 'e_R'.
-        # We do the same thing for 'mu', 'tau', 'W' and 'Z'.
-        # To avoid thinking too much, all spectra are split into two parts.
-        # self._weight gives the weight of each half.
-            
-        if pri == 'e' or pri == 'mu' or pri == 'tau':
-            pri_1 = pri + '_L'
-            pri_2 = pri + '_R'
-            self._weight = [0.5, 0.5]
-        elif pri == 'W' or pri == 'Z':
-            # 2 transverse pol., 1 longitudinal.
-            pri_1 = pri + '_T'
-            pri_2 = pri + '_L'
-            self._weight = [2/3, 1/3]
-        else:
-            pri_1 = pri
-            pri_2 = pri
-            self._weight = [0.5, 0.5]
-        
-        # Compile the raw data.
-        mDM_in_GeV_arr_1 = np.array(
-            coords_data[i, idx_list_data[pri_1], 0]
-        )
-        log10x_arr_1     = np.array(
-            coords_data[i, idx_list_data[pri_1], 1]
-        )
-        values_arr_1     = np.array(values_data[i, idx_list_data[pri_1]])
-
-        mDM_in_GeV_arr_2 = np.array(
-            coords_data[i, idx_list_data[pri_2], 0]
-        )
-        log10x_arr_2     = np.array(
-            coords_data[i, idx_list_data[pri_2], 1]
-        )
-        values_arr_2     = np.array(values_data[i, idx_list_data[pri_2]])
-
-        self._mDM_in_GeV_arrs = [mDM_in_GeV_arr_1, mDM_in_GeV_arr_2] 
-        self._log10x_arrs     = [log10x_arr_1,     log10x_arr_2]
-
-        # Save the 1D PCHIP interpolator over mDM_in_GeV. Multiply the 
-        # electron spectrum by 2 by adding np.log10(2).  
-        self._interpolators = [
-            PchipInterpolator(
-                mDM_in_GeV_arr_1, values_arr_1 + np.log10(fac), 
-                extrapolate=False
-            ),
-            PchipInterpolator(
-                mDM_in_GeV_arr_2, values_arr_2 + np.log10(fac),
-                extrapolate=False
-            )
-        ]
-    
-    def get_val(self, mDM_in_GeV, log10x):
-        
-        if (
-            mDM_in_GeV < self._mDM_in_GeV_arrs[0][0] 
-            or mDM_in_GeV < self._mDM_in_GeV_arrs[1][0]
-            or mDM_in_GeV > self._mDM_in_GeV_arrs[0][-1]
-            or mDM_in_GeV > self._mDM_in_GeV_arrs[1][-1]
-        ):
-            raise TypeError('mDM lies outside of the interpolation range.')
-        
-        # Call the saved interpolator at mDM_in_GeV, 
-        # then use PCHIP 1D interpolation at log10x. 
-        result1 = pchip_interpolate(
-            self._log10x_arrs[0], self._interpolators[0](mDM_in_GeV), log10x
-        )
-        # Set all values outside of the log10x interpolation range to 
-        # (effectively) zero. 
-        result1[log10x >= self._log10x_arrs[0][-1]] = -100.
-        result1[log10x <= self._log10x_arrs[0][0]]  = -100.
-        
-        result2 = pchip_interpolate(
-            self._log10x_arrs[1], self._interpolators[1](mDM_in_GeV), log10x
-        )
-        result2[log10x >= self._log10x_arrs[1][-1]] = -100.
-        result2[log10x <= self._log10x_arrs[1][0]]  = -100.
-        
-        # Combine the two spectra.  
-        return np.log10(
-            self._weight[0]*10**result1 + self._weight[1]*10**result2
-        )
-
-# This list includes 'e', 'mu', 'tau', 'W' and 'Z'. 
-chan_list = [
-    'e_L','e_R', 'e', 'mu_L', 'mu_R', 'mu', 'tau_L', 'tau_R', 'tau',
-    'q',  'c',  'b', 't',
-    'W_L', 'W_T', 'W', 'Z_L', 'Z_T', 'Z', 'g',  'gamma', 'h',
-    'nu_e', 'nu_mu', 'nu_tau',
-    'VV_to_4e', 'VV_to_4mu', 'VV_to_4tau'
-]
 
 # Mass threshold for mDM to annihilate into the primaries.
 mass_threshold = {
@@ -213,12 +206,12 @@ mass_threshold = {
 }
 
 # Compile a dictionary of all of the interpolators.
-dlNdlxIEW_interp = {'elec':{}, 'phot':{}}
+# dlNdlxIEW_interp = {'elec':{}, 'phot':{}}
 
-if 'pytest' not in sys.modules and 'readthedocs' not in sys.modules: 
-    for pri in chan_list:
-        dlNdlxIEW_interp['elec'][pri] = PchipInterpolator2D(pri, 'elec')
-        dlNdlxIEW_interp['phot'][pri] = PchipInterpolator2D(pri, 'phot')
+# if 'pytest' not in sys.modules and 'readthedocs' not in sys.modules: 
+#     for pri in chan_list:
+#         dlNdlxIEW_interp['elec'][pri] = PchipInterpolator2D(pri, 'elec')
+#         dlNdlxIEW_interp['phot'][pri] = PchipInterpolator2D(pri, 'phot')
 
 def get_pppc_spec(mDM, eng, pri, sec, decay=False):
 
@@ -335,9 +328,12 @@ def get_pppc_spec(mDM, eng, pri, sec, decay=False):
                 np.arange(log10x.size), 
                 log10x
             )
-    
-    # Get the spectrum for the interpolator.
-    dN_dlog10x = 10**dlNdlxIEW_interp[sec][pri].get_val(_mDM/1e9, log10x)
+    # Get the interpolator. 
+    dlNdlxIEW_interp = get_data('pppc', primary=pri)
+
+    # Get the spectrum from the interpolator.
+    dN_dlog10x = 10**dlNdlxIEW_interp[sec].get_val(_mDM/1e9, log10x)
+    # dN_dlog10x = 10**dlNdlxIEW_interp[sec][pri].get_val(_mDM/1e9, log10x)
     
     # Recall that dN/dE = dN/dlog10x * dlog10x/dE
     x = 10**log10x
