7799ef6f9dd77b371c3f7f2802d69d6c304e1242
Hongwan Liu <hongwan@mit.edu>
2019-04-13 00:50:00 -0400

    C_He and coarsening bugs fixed
    * C_He returned nan for the triplet state due to a very small exponential.
    * Coarsening must not choose a target redshift below 4.

--- main.py
+++ main.py
@@ -756,8 +756,10 @@ def get_tf(rs, xHII, xHeII, dlnz, coarsen_factor=1):
     highengdep_interp     = dep_tf_data['highengdep']
 
     if coarsen_factor > 1:
-        # rs_to_interpolate = rs
-        rs_to_interpolate = np.exp(np.log(rs) - dlnz * coarsen_factor/2)
+        # Ensures that it doesn't fall off the edge. 
+        rs_to_interpolate = np.max(
+            (np.exp(np.log(rs) - dlnz * coarsen_factor/2), 4.)
+        )
     else:
         rs_to_interpolate = rs
 
