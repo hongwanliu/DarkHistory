20aa55a46ee4981e09c3ae479f513c5df2949c71
Wenzer Qin <viviesque@ctpaquamarinelnsmitedu.local>
2019-09-25 21:47:01 -0400

    changed personal config

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ from scipy.interpolate import RegularGridInterpolator
 # Location of all data files. CHANGE THIS FOR DARKHISTORY TO ALWAYS
 # LOOK FOR THESE DATA FILES HERE. 
 
-data_path = '/foo/bar'
+data_path = '/Users/viviesque/OneDrive - Massachusetts Institute of Technology/DarkHistory/data'
+#data_path = '/Users/wenze/OneDrive - Massachusetts Institute of Technology/DarkHistory/data'
 
 # Global variables for data.
 glob_binning_data = None
