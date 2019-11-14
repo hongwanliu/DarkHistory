4003c05f8c83411147b7fc45019f7f03f3b028cb
Gregory Ridgway <gregridgway@gmail.com>
2019-11-14 16:52:37 -0500

    Included new options in reionization when reionization method is None

--- darkhistory/history/reionization.py
+++ darkhistory/history/reionization.py
@@ -20,6 +20,8 @@ def photoion_rate(species, method = 'Puchwein'):
     function
         Interpolating function which takes redshift as an argument, and returns the photoionization rate of the species in s^-1. See 1801.04931, and 1607.04218 for early/middle/late. 
     """
+    if method == None:
+        return None
     if method == 'Puchwein':
         rs_vec = 1. + np.array([
             0, 0.0491, 0.101, 0.155, 0.211, 0.271, 0.333, 0.399, 0.468, 0.54, 
@@ -90,6 +92,8 @@ def photoheat_rate(species, method='Puchwein'):
     function
         Interpolating function which takes redshift as an argument, and returns the photoheating rate of the species in s^-1. See 1801.04931 for Puchwein, and 1607.04218 for early/middle/late.
     """
+    if method == None:
+        return None
     if method == 'Puchwein':
         rs_vec = 1. + np.array([
             0, 0.0491, 0.101, 0.155, 0.211, 0.271, 0.333, 0.399, 0.468, 0.54, 
