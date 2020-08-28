30db9f02a9e7c5ac378c528498385a7b470540be
Wenzer Qin <wenzer.qin@gmail.com>
2020-08-28 10:21:06 -0400

    I'm not sure what I changed but I have to do this before switching branches

--- config.py
+++ config.py
@@ -18,10 +18,7 @@ from scipy.interpolate import interp1d
 # Location of all data files. CHANGE THIS FOR DARKHISTORY TO ALWAYS
 # LOOK FOR THESE DATA FILES HERE. 
 
-#data_path = '/Users/hongwan/Dropbox (MIT)/Photon Deposition/DarkHistory_data'
-data_path = '/Users/gregoryridgway/Downloads/dataverse_files_06_08_2019/'
-#data_path = '/Users/gridgway/Downloads/dataverse_files/'
-
+data_path = '/Users/viviesque/OneDrive - Massachusetts Institute of Technology/DarkHistory/data'
 
 # Global variables for data.
 glob_binning_data = None
