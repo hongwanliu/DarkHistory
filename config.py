2b02d7937d0df65569d74b2aa46d935751db8905
Wenzer Qin <wenzer.qin@gmail.com>
2020-08-29 11:24:29 -0400

    added my data_path to config.py as a test

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ from scipy.interpolate import interp1d
 #data_path = '/Users/hongwan/Dropbox (MIT)/Photon Deposition/DarkHistory_data'
 data_path = '/Users/gregoryridgway/Downloads/dataverse_files_06_08_2019/'
 #data_path = '/Users/gridgway/Downloads/dataverse_files/'
-
+#data_path = '/Users/viviesque/OneDrive - Massachusetts Institute of Technology/DarkHistory/data/'
 
 # Global variables for data.
 glob_binning_data = None
