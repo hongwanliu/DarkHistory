dbb0fbf83da7eca28e9ad964af262e015d293e79
Yitian Sun <yitian.sun.26@gmail.com>
2024-06-07 14:17:37 -0400

    revive pytest

--- tests/test_spectrum.py
+++ tests/test_spectrum.py
@@ -1,7 +1,9 @@
-import numpy as np
-
+import os
+import sys
 from pytest import approx
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import numpy as np
 from darkhistory.spec.spectrum import Spectrum
 
 def test_redshift():
